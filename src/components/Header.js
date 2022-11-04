a972d5211e071ec6f94ef4c4065d5fd871462a45
John Kim <joki9448@gmail.com>
2022-11-04 16:50:54 -0400

    finished styling header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ function Header() {
     return (
         <header className="header">
             <img src="fetchlogo.jpg" alt="logo"></img>
+            <h2>Welcome!</h2>
         </header>
     )
 }
