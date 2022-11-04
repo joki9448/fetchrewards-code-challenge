d371218f73986f9283e70f49a0c0d26b5fa2c748
John Kim <joki9448@gmail.com>
2022-11-03 21:43:10 -0400

    created base components

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer () {
+    return (
+        <footer>
+            <h1>This is the footer</h1>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Form.js
@@ -0,0 +1,10 @@
+function Form () {
+    
+    return (
+        <form>
+            <h1>This is the form</h1>
+        </form>
+    )
+}
+
+export default Form;
